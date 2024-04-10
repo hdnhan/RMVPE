f36f41af840a2ba6e88d174fb87c70f101945dc0
Nhan Ho <hdnhan28@gmail.com>
2024-04-10 17:02:17 +0700

    improved

--- src/deepunet.py
+++ src/deepunet.py
@@ -1,45 +1,53 @@
+from typing import Tuple, List, Optional, Union
+
 import torch
 import torch.nn as nn
 from .constants import N_MELS
 
 
 class ConvBlockRes(nn.Module):
-    def __init__(self, in_channels, out_channels, momentum=0.01):
+    def __init__(self, in_channels: int, out_channels: int, momentum: float = 0.01):
         super(ConvBlockRes, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels,
-                      out_channels=out_channels,
-                      kernel_size=(3, 3),
-                      stride=(1, 1),
-                      padding=(1, 1),
-                      bias=False),
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=(3, 3),
+                stride=(1, 1),
+                padding=(1, 1),
+                bias=False,
+            ),
             nn.BatchNorm2d(out_channels, momentum=momentum),
             nn.ReLU(),
-
-            nn.Conv2d(in_channels=out_channels,
-                      out_channels=out_channels,
-                      kernel_size=(3, 3),
-                      stride=(1, 1),
-                      padding=(1, 1),
-                      bias=False),
+            nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=(3, 3),
+                stride=(1, 1),
+                padding=(1, 1),
+                bias=False,
+            ),
             nn.BatchNorm2d(out_channels, momentum=momentum),
             nn.ReLU(),
         )
         if in_channels != out_channels:
             self.shortcut = nn.Conv2d(in_channels, out_channels, (1, 1))
-            self.is_shortcut = True
         else:
-            self.is_shortcut = False
+            self.shortcut = nn.Identity()
 
-    def forward(self, x):
-        if self.is_shortcut:
-            return self.conv(x) + self.shortcut(x)
-        else:
-            return self.conv(x) + x
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.conv(x) + self.shortcut(x)
 
 
 class ResEncoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, n_blocks=1, momentum=0.01):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Optional[Tuple[int, int]],
+        n_blocks: int = 1,
+        momentum: int = 0.01,
+    ) -> None:
         super(ResEncoderBlock, self).__init__()
         self.n_blocks = n_blocks
         self.conv = nn.ModuleList()
@@ -50,9 +58,10 @@ class ResEncoderBlock(nn.Module):
         if self.kernel_size is not None:
             self.pool = nn.AvgPool2d(kernel_size=kernel_size)
 
-    def forward(self, x):
-        for i in range(self.n_blocks):
-            x = self.conv[i](x)
+    def forward(self, x: torch.Tensor):
+        for c in self.conv:
+            x = c(x)
+
         if self.kernel_size is not None:
             return x, self.pool(x)
         else:
@@ -60,43 +69,57 @@ class ResEncoderBlock(nn.Module):
 
 
 class ResDecoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, n_blocks=1, momentum=0.01):
+    def __init__(
+        self, in_channels: int, out_channels: int, stride: int, n_blocks: int = 1, momentum: float = 0.01
+    ) -> None:
         super(ResDecoderBlock, self).__init__()
         out_padding = (0, 1) if stride == (1, 2) else (1, 1)
-        self.n_blocks = n_blocks
         self.conv1 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=in_channels,
-                               out_channels=out_channels,
-                               kernel_size=(3, 3),
-                               stride=stride,
-                               padding=(1, 1),
-                               output_padding=out_padding,
-                               bias=False),
+            nn.ConvTranspose2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=(3, 3),
+                stride=stride,
+                padding=(1, 1),
+                output_padding=out_padding,
+                bias=False,
+            ),
             nn.BatchNorm2d(out_channels, momentum=momentum),
             nn.ReLU(),
         )
         self.conv2 = nn.ModuleList()
         self.conv2.append(ConvBlockRes(out_channels * 2, out_channels, momentum))
-        for i in range(n_blocks-1):
+        for i in range(n_blocks - 1):
             self.conv2.append(ConvBlockRes(out_channels, out_channels, momentum))
 
-    def forward(self, x, concat_tensor):
+    def forward(self, x: torch.Tensor, concat_tensor: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
         x = torch.cat((x, concat_tensor), dim=1)
-        for i in range(self.n_blocks):
-            x = self.conv2[i](x)
+        for c in self.conv2:
+            x = c(x)
         return x
 
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels, in_size, n_encoders, kernel_size, n_blocks, out_channels=16, momentum=0.01):
+    def __init__(
+        self,
+        in_channels: int,
+        in_size: int,
+        n_encoders: int,
+        kernel_size: Tuple[int, int],
+        n_blocks: int,
+        out_channels: int = 16,
+        momentum: float = 0.01,
+    ):
         super(Encoder, self).__init__()
         self.n_encoders = n_encoders
         self.bn = nn.BatchNorm2d(in_channels, momentum=momentum)
         self.layers = nn.ModuleList()
         self.latent_channels = []
         for i in range(self.n_encoders):
-            self.layers.append(ResEncoderBlock(in_channels, out_channels, kernel_size, n_blocks, momentum=momentum))
+            self.layers.append(
+                ResEncoderBlock(in_channels, out_channels, kernel_size, n_blocks, momentum=momentum)
+            )
             self.latent_channels.append([out_channels, in_size])
             in_channels = out_channels
             out_channels *= 2
@@ -104,27 +127,29 @@ class Encoder(nn.Module):
         self.out_size = in_size
         self.out_channel = out_channels
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, List[torch.Tensor]]:
         concat_tensors = []
         x = self.bn(x)
-        for i in range(self.n_encoders):
-            _, x = self.layers[i](x)
+        for layer in self.layers:
+            _, x = layer(x)
             concat_tensors.append(_)
         return x, concat_tensors
 
 
 class Intermediate(nn.Module):
-    def __init__(self, in_channels, out_channels, n_inters, n_blocks, momentum=0.01):
+    def __init__(
+        self, in_channels: int, out_channels: int, n_inters: int, n_blocks: int, momentum: float = 0.01
+    ) -> None:
         super(Intermediate, self).__init__()
         self.n_inters = n_inters
         self.layers = nn.ModuleList()
         self.layers.append(ResEncoderBlock(in_channels, out_channels, None, n_blocks, momentum))
-        for i in range(self.n_inters-1):
+        for i in range(self.n_inters - 1):
             self.layers.append(ResEncoderBlock(out_channels, out_channels, None, n_blocks, momentum))
 
-    def forward(self, x):
-        for i in range(self.n_inters):
-            x = self.layers[i](x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.layers:
+            x = layer(x)
         return x
 
 
@@ -138,9 +163,9 @@ class Decoder(nn.Module):
             self.layers.append(ResDecoderBlock(in_channels, out_channels, stride, n_blocks, momentum))
             in_channels = out_channels
 
-    def forward(self, x, concat_tensors):
-        for i in range(self.n_decoders):
-            x = self.layers[i](x, concat_tensors[-1-i])
+    def forward(self, x: torch.Tensor, concat_tensors: List[torch.Tensor]) -> torch.Tensor:
+        for i, layer in enumerate(self.layers):
+            x = layer(x, concat_tensors[-1 - i])
         return x
 
 
@@ -151,7 +176,7 @@ class TimbreFilter(nn.Module):
         for latent_rep in latent_rep_channels:
             self.layers.append(ConvBlockRes(latent_rep[0], latent_rep[0]))
 
-    def forward(self, x_tensors):
+    def forward(self, x_tensors: List[torch.Tensor]) -> List[torch.Tensor]:
         out_tensors = []
         for i, layer in enumerate(self.layers):
             out_tensors.append(layer(x_tensors[i]))
@@ -159,30 +184,44 @@ class TimbreFilter(nn.Module):
 
 
 class DeepUnet(nn.Module):
-    def __init__(self, kernel_size, n_blocks, en_de_layers=5, inter_layers=4, in_channels=1, en_out_channels=16):
+    def __init__(
+        self, kernel_size, n_blocks, en_de_layers=5, inter_layers=4, in_channels=1, en_out_channels=16
+    ):
         super(DeepUnet, self).__init__()
         self.encoder = Encoder(in_channels, N_MELS, en_de_layers, kernel_size, n_blocks, en_out_channels)
-        self.intermediate = Intermediate(self.encoder.out_channel // 2, self.encoder.out_channel, inter_layers, n_blocks)
+        self.intermediate = Intermediate(
+            self.encoder.out_channel // 2, self.encoder.out_channel, inter_layers, n_blocks
+        )
         self.tf = TimbreFilter(self.encoder.latent_channels)
         self.decoder = Decoder(self.encoder.out_channel, en_de_layers, kernel_size, n_blocks)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x, concat_tensors = self.encoder(x)
         x = self.intermediate(x)
         concat_tensors = self.tf(concat_tensors)
         x = self.decoder(x, concat_tensors)
         return x
 
-      
+
 class DeepUnet0(nn.Module):
-    def __init__(self, kernel_size, n_blocks, en_de_layers=5, inter_layers=4, in_channels=1, en_out_channels=16):
+    def __init__(
+        self,
+        kernel_size: Tuple[int, int],
+        n_blocks: int,
+        en_de_layers: int = 5,
+        inter_layers: int = 4,
+        in_channels: int = 1,
+        en_out_channels: int = 16,
+    ) -> None:
         super(DeepUnet0, self).__init__()
         self.encoder = Encoder(in_channels, N_MELS, en_de_layers, kernel_size, n_blocks, en_out_channels)
-        self.intermediate = Intermediate(self.encoder.out_channel // 2, self.encoder.out_channel, inter_layers, n_blocks)
+        self.intermediate = Intermediate(
+            self.encoder.out_channel // 2, self.encoder.out_channel, inter_layers, n_blocks
+        )
         self.tf = TimbreFilter(self.encoder.latent_channels)
         self.decoder = Decoder(self.encoder.out_channel, en_de_layers, kernel_size, n_blocks)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x, concat_tensors = self.encoder(x)
         x = self.intermediate(x)
         x = self.decoder(x, concat_tensors)
--- src/seq.py
+++ src/seq.py
@@ -1,20 +1,26 @@
+import torch
 import torch.nn as nn
 
 
 class BiGRU(nn.Module):
-    def __init__(self, input_features, hidden_features, num_layers):
+    def __init__(self, input_features: int, hidden_features: int, num_layers: int) -> None:
         super(BiGRU, self).__init__()
-        self.gru = nn.GRU(input_features, hidden_features, num_layers=num_layers, batch_first=True, bidirectional=True)
+        self.gru = nn.GRU(
+            input_features, hidden_features, num_layers=num_layers, batch_first=True, bidirectional=True
+        )
 
-    def forward(self, x):
-        return self.gru(x)[0]
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.gru.cpu()
+
+        return self.gru(x.cpu())[0].to(x.device)
 
 
 class BiLSTM(nn.Module):
-    def __init__(self, input_features, hidden_features, num_layers):
+    def __init__(self, input_features: int, hidden_features: int, num_layers: int) -> None:
         super(BiLSTM, self).__init__()
-        self.lstm = nn.LSTM(input_features, hidden_features, num_layers=num_layers, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(
+            input_features, hidden_features, num_layers=num_layers, batch_first=True, bidirectional=True
+        )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.lstm(x)[0]
-
